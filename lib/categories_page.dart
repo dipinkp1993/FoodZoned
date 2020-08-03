0e7dbae2c10c2b4e301c2febed9f62740f71ddb7
Dipin KP <dipinpnambiar@gmail.com>
2020-08-03 23:16:38 +0530

    Category item is added with category_item.dart

--- lib/categories_page.dart
+++ lib/categories_page.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
+import './category_item.dart';
 
 class CategoriesPage extends StatelessWidget {
   //This page controls entire content in the device of this app
   @override
   Widget build(BuildContext context) {
     return GridView(
+        children: DUMMY_CATEGORIES
+            .map((catItem) => CategoryItem(catItem.title, catItem.color))
+            .toList(),
         gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
-      maxCrossAxisExtent: 200,
-      childAspectRatio: 3 / 2,
-      crossAxisSpacing: 20,
-      mainAxisSpacing: 20,
-    ));
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ));
   }
 }
